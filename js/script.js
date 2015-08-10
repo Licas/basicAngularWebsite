c1ccc8690ca19fd74c2532bc2c22996a60bef6aa
Manuel Morini <morinimanuel@gmail.com>
2015-08-10 22:40:07 +0200

    Added basic routing and controller

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Code goes here
+
+var app = angular.module("computersolution", ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+
+  $routeProvider
+   .when('/main', {
+     templateUrl:'main.html',
+     controller: 'MainCtrl'
+   })
+   .otherwise({ redirectTo:'main' });
+
+}])
+
+.controller('MainCtrl',  ['$scope',function($scope){
+  $scope.person = 'John Doe';
+  console.log($scope);
+
+}]);
